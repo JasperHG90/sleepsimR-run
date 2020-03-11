6e60241114ffbdbc9f0b7ca9a881da3cb3efea2b
Jasper Ginn <jasperginn@gmail.com>
2020-03-11 20:53:52 +0100

    Add argparse arguments

--- app/run_sleepsimR.R
+++ app/run_sleepsimR.R
@@ -5,7 +5,8 @@ library(argparser, quietly=TRUE)
 # Set up a parser
 args <- arg_parser("Simulate normalized EEG/EOG data and run a multilevel hidden markov model (mHMM)")
 # Add arguments
-args <- add_argument(args, "--host", help="Host address on which the sleepsimR API is running.", default="http://localhost:5007")
-args <- add_argument(args, "api-key", help="API key used to communicate with the API.")
-# Parse 
+args <- add_argument(args, "username", help="User name used to authenticate with the API.")
+args <- add_argument(args, "password", help="Password used to authenticate with the API.")
+args <- add_argument(args, "--host", help="Host address on which the sleepsimR API is running.", default="http://localhost:5002")
+# Parse
 argv <- parse_args(args)
